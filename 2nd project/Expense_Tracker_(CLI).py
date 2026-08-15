3320f85b9c5c42b0e4c49cbd42627a3da1c30d46
Daksh Sharma <dakshshrama82@gmail.com>
2026-08-15 13:10:35 +0530

    Refactor expense tracker CLI to encapsulate functionality within a function

--- 2nd project/Expense_Tracker_(CLI).py
+++ 2nd project/Expense_Tracker_(CLI).py
@@ -4,31 +4,36 @@ totalmoney = []
 print("wellcome to CLI:\n","choose the menu option:")
 menu = ["adding exp","view exp","total","remove exp"]
 print(menu)
-while True:
-    task = input("enter the perpouse: ")
-    if task == "adding exp":
-        a = int(input("enter the value: "))
-        while a < 0 :
+def function_list():
+    while True:
+        task = input("enter the perpouse: ")
+        if task == "adding exp":
+            a = int(input("enter the value: "))
+            while a < 0 :
                  print("wrong value")
                  a = int(input("enter the value: "))
-        b = input("enter the catagory :")
-        billbook[b] = a
-        totalexpens.append(billbook)
-        totalmoney.append(a)
-    elif task == "view exp":
-        if totalexpens == [] :
-            print("nothing is listed")
-        else:
-            print(totalexpens)
-    elif task == "total":
-         print(sum(totalmoney))
-    elif task == "remove exp":
-        if totalexpens == []:
-            print("nothing in the list")
-        elif totalexpens != []:
-            print(totalexpens)
-            remove = int(input("enter the entry no:"))
-            totalexpens.remove(remove)
-    if task == "exit":
-        break
-print("thanks for using")
+            b = input("enter the catagory :")
+            billbook[b] = a
+            done = input("done adding all exp(yes/no):")
+            if done == "yes":
+                totalexpens.append(billbook)
+                totalmoney.append(a)
+            else:
+                pass
+        elif task == "view exp":
+            if totalexpens == [] :
+                print("nothing is listed")
+            else:
+                 print(totalexpens)
+        elif task == "total":
+             print(sum(totalmoney))
+        elif task == "remove exp":
+            if totalexpens == []:
+              print("nothing in the list")
+            elif totalexpens != []:
+                print(totalexpens)
+                remove = (input("enter the entry no:"))
+                totalexpens.remove(remove)
+        if task == "exit":
+            break
+function_list()

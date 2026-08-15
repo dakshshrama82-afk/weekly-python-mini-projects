445499dffc61bcee5e3f8d9a5c7265eaa0b8074c
Daksh Sharma <dakshshrama82@gmail.com>
2026-08-15 21:50:28 +0530

    making multi function and resolving some bug

--- 2nd project/Expense_Tracker_(CLI).py
+++ 2nd project/Expense_Tracker_(CLI).py
@@ -1,39 +1,73 @@
+#main billing book and total exp defien
+
 billbook = {}
-totalexpens = []
+bill_log = []
 totalmoney = []
+
+#printing wellcom and option menu
+
 print("wellcome to CLI:\n","choose the menu option:")
 menu = ["adding exp","view exp","total","remove exp"]
 print(menu)
-def function_list():
-    while True:
-        task = input("enter the perpouse: ")
-        if task == "adding exp":
-            a = int(input("enter the value: "))
-            while a < 0 :
-                 print("wrong value")
-                 a = int(input("enter the value: "))
-            b = input("enter the catagory :")
-            billbook[b] = a
-            done = input("done adding all exp(yes/no):")
-            if done == "yes":
-                totalexpens.append(billbook)
-                totalmoney.append(a)
-            else:
-                pass
-        elif task == "view exp":
-            if totalexpens == [] :
-                print("nothing is listed")
-            else:
-                 print(totalexpens)
-        elif task == "total":
-             print(sum(totalmoney))
-        elif task == "remove exp":
-            if totalexpens == []:
-              print("nothing in the list")
-            elif totalexpens != []:
-                print(totalexpens)
-                remove = (input("enter the entry no:"))
-                totalexpens.remove(remove)
-        if task == "exit":
-            break
-function_list()
+
+#making a function for adding expense
+
+def adding_exp():
+    a = float(input("enter the value: "))
+    while a < 0:
+        print("wrong value")
+        a = float(input("enter the value: "))
+    b = input("enter the catagory :").lower()
+    billbook[b] = a
+    done = input("done adding all exp(yes/no):")
+    if done == "yes":
+        totalmoney.append(a)
+    else:
+        adding_exp()
+
+# making a function for viewing billbook
+
+def view_exp():
+    if billbook == {}:
+        print("nothing found")
+    else:
+        print(billbook)
+
+#making a function for total expense
+
+def total_exp():
+    if totalmoney == []:
+        print("nothing found")
+    else:
+        print(sum(totalmoney))
+
+#making a function for removing exp
+
+def removing_exp():
+    if billbook == {}:
+        print("nothing in it")
+    else:
+        print(billbook)
+        remove = input("enter the task to remove:")
+        billbook.pop(remove)
+        totalmoney.append(billbook)
+        print(billbook)
+
+# looping all function (also you can call it the heart of this program)
+
+while True:
+    user_task = input("enter the task:").lower()
+    if user_task == "adding" or user_task == "adding exp":
+        adding_exp()
+    elif user_task == "view" or user_task == "view exp":
+        view_exp()
+    elif user_task == "total_mony" or user_task == "total":
+        total_exp()
+    elif user_task == "removing":
+        removing_exp()
+    elif user_task == "exit":
+        break
+    else:
+        print("option does not found")
+
+
